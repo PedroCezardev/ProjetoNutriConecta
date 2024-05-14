146ce581bb07bd3d4490e7e7f9fb2887ddf471f4
PedroCezardev <pcdasilvabeserra@gmail.com>
2024-05-14 10:08:41 -0300

    adicionando primeiros arquivos

---
+++ NutriConecta/src/Pages/Disclosures/Disclosures.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Disclosures = () => {
+  return (
+    <div>Disclosures</div>
+  )
+}
+
+export default Disclosures
---
+++ NutriConecta/src/Pages/Publication/Publication.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Publication = () => {
+  return (
+    <div>Publication</div>
+  )
+}
+
+export default Publication
